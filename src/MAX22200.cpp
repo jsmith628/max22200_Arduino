45367201d91f2e95ae3bea45e6bb405f4324f86e
Joshua Smith <jsmith62831@gmail.com>
2022-11-13 22:36:43 -0600

    created the base class

--- src/MAX22200.cpp
+++ src/MAX22200.cpp
@@ -1,2 +1,36 @@
 
 #include "MAX22200.h"
+
+#include <Arduino.h>
+#include <SPI.h>
+
+MAX22200::MAX22200(uint8_t en, uint8_t csb, uint8_t cmd): MAX22200(en, csb, cmd, 1) {}
+
+MAX22200::MAX22200(uint8_t en, uint8_t csb, uint8_t cmd, uint8_t count) {
+    
+    //set pins
+    pin_en = en;
+    pin_csb = csb;
+    pin_cmd = cmd;
+
+    //set to outputs
+    pinMode(pin_en, OUTPUT);
+    pinMode(pin_csb, OUTPUT);
+    pinMode(pin_cmd, OUTPUT);
+
+    this->count = count;
+    
+}
+
+
+inline void MAX22200::beginTransaction() const {
+    SPI.beginTransaction(SPISettings(5e6/*5MHz*/, MSBFIRST, SPI_MODE0));
+}
+
+inline void MAX22200::endTransaction() const {
+    SPI.endTransaction();
+}
+
+inline void MAX22200::enable() { digitalWrite(pin_en, true); }
+inline void MAX22200::disable() { digitalWrite(pin_en, false); }
+inline void MAX22200::setEnable(bool en) { digitalWrite(pin_en, en); }
--- src/MAX22200.h
+++ src/MAX22200.h
@@ -1,4 +1,31 @@
 #ifndef MAX22200_H
 #define MAX22200_H
 
+#include <stdint.h>
+
+class MAX22200 {
+
+private:
+
+    uint8_t pin_en;
+    uint8_t pin_csb;
+    uint8_t pin_cmd;
+    uint8_t pin_fault;
+    
+    uint8_t count;
+
+    inline void beginTransaction() const;
+    inline void endTransaction() const;
+
+public:
+
+    MAX22200(uint8_t en, uint8_t csb, uint8_t cmd);
+    MAX22200(uint8_t en, uint8_t csb, uint8_t cmd, uint8_t count);
+
+    inline void enable();
+    inline void disable();
+    inline void setEnable(bool);
+
+};
+
 #endif //MAX22200_H
