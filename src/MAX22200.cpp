23818087c1a54b7d72f8e23fbf9e5e4f31e3cb5f
Joshua Smith <jsmith62831@gmail.com>
2023-08-11 20:18:26 -0400

    redid archetecture for single chip operation

--- src/MAX22200.cpp
+++ src/MAX22200.cpp
@@ -1,12 +1,11 @@
 
 #include "MAX22200.h"
+#include "MAX22200_registers.h"
 
 #include <Arduino.h>
 #include <SPI.h>
 
-MAX22200::MAX22200(uint8_t en, uint8_t csb, uint8_t cmd): MAX22200(en, csb, cmd, 1) {}
-
-MAX22200::MAX22200(uint8_t en, uint8_t csb, uint8_t cmd, uint8_t count) {
+MAX22200::MAX22200(uint8_t en, uint8_t csb, uint8_t cmd) {
     
     //set pins
     pin_en = en;
@@ -20,87 +19,73 @@ MAX22200::MAX22200(uint8_t en, uint8_t csb, uint8_t cmd, uint8_t count) {
 
     digitalWrite(pin_csb, HIGH);
     digitalWrite(pin_cmd, LOW);
-
-    this->count = count;
     
 }
 
-
 inline void MAX22200::beginTransaction() {
     SPI.beginTransaction(SPISettings(5e6/*5MHz*/, MSBFIRST, SPI_MODE0));
-    digitalWrite(pin_csb, LOW);
 }
 
 inline void MAX22200::endTransaction() {
-    digitalWrite(pin_csb, HIGH);
     SPI.endTransaction();
 }
 
-inline void MAX22200::enable() { digitalWrite(pin_en, true); }
-inline void MAX22200::disable() { digitalWrite(pin_en, false); }
-inline void MAX22200::setEnable(bool en) { digitalWrite(pin_en, en); }
-
 inline void MAX22200::sendCmd(uint8_t cmd) {
-
     digitalWrite(pin_cmd, HIGH);
-    beginTransaction();
-
-    for(int i=0; i<count; i++) {
-        SPI.transfer(cmd);
-    }
-
-    endTransaction();
+    digitalWrite(pin_csb, LOW);
+    SPI.transfer(cmd);
+    digitalWrite(pin_csb, HIGH); //TODO: check if this is necessary
     digitalWrite(pin_cmd, LOW);
-
 }
 
-inline void MAX22200::read(uint8_t addr, uint8_t* data) {
+inline void MAX22200::setEnable(bool en) { digitalWrite(pin_en, en); }
+inline void MAX22200::enable() { setEnable(true); }
+inline void MAX22200::disable() { setEnable(false); }
 
-    uint8_t cmd = MAX22200_COMMAND(true, addr, MAX22200_READ);
-    sendCmd(cmd);
-
+inline uint8_t MAX22200::transfer8(uint8_t addr, uint8_t out) {
     beginTransaction();
-    for(int i=count-1; i>=0; i--) {
-        data[i] = SPI.transfer(0);
-    }
+
+    sendCmd(MAX22200_COMMAND(true, addr, MAX22200_READ));    
+
+    digitalWrite(pin_csb, LOW);
+    uint8_t in = SPI.transfer(0);
+    digitalWrite(pin_csb, HIGH);
+    
     endTransaction();
 
+    return in;
 }
 
-inline void MAX22200::read(uint8_t addr, uint32_t* data) {
-
-    uint8_t cmd = MAX22200_COMMAND(false, addr, MAX22200_READ);
-    sendCmd(cmd);
-
+inline uint32_t MAX22200::transfer32(uint8_t addr, uint32_t out) {
     beginTransaction();
-    for(int i=count-1; i>=0; i--) {
-        data[i] = 0;
-        data[i] |= ((uint32_t) SPI.transfer16(0)) << 16;
-        data[i] |= ((uint32_t) SPI.transfer16(0)) & 0xFFFF;
-    }
+
+    sendCmd(MAX22200_COMMAND(true, addr, MAX22200_READ));    
+
+    digitalWrite(pin_csb, LOW);
+    uint32_t in = 0;
+    in |= SPI.transfer((uint8_t) (out));
+    in |= SPI.transfer((uint8_t) (out>>8));
+    in |= SPI.transfer((uint8_t) (out>>16));
+    in |= SPI.transfer((uint8_t) (out>>24));
+    digitalWrite(pin_csb, HIGH);
+    
     endTransaction();
 
+    return in;
 }
 
-inline void MAX22200::write(uint8_t addr, const uint8_t* data) {
-    uint8_t cmd = MAX22200_COMMAND(true, addr, MAX22200_WRITE);
-    sendCmd(cmd);
-
-    beginTransaction();
-    for(int i=count-1; i>=0; i--) {
-        SPI.transfer(data[i]);
-    }
-    endTransaction();
+inline uint8_t MAX22200::read8(uint8_t addr) {
+    return transfer8(addr, 0);
 }
 
-inline void MAX22200::write(uint8_t addr, const uint32_t* data) {
-    uint8_t cmd = MAX22200_COMMAND(false, addr, MAX22200_WRITE);
-    sendCmd(cmd);
+inline uint32_t MAX22200::read32(uint8_t addr) {
+    return transfer32(addr, 0);
+}
 
-    beginTransaction();
-    for(int i=count-1; i>=0; i--) {
-        SPI.transfer16((uint16_t) (data[i] >> 16));
-        SPI.transfer16((uint16_t) (data[i] & 0xFFFF));
-    }
-    endTransaction();
+inline void MAX22200::write8(uint8_t addr, uint8_t data) {
+    transfer8(addr, data);
+}
+
+inline void MAX22200::write32(uint8_t addr, uint32_t data) {
+    transfer32(addr, data);
 }
--- src/MAX22200.h
+++ src/MAX22200.h
@@ -1,8 +1,6 @@
 #ifndef MAX22200_H
 #define MAX22200_H
 
-#include "MAX22200_registers.h"
-
 #include <stdint.h>
 
 class MAX22200 {
@@ -13,28 +11,28 @@ private:
     uint8_t pin_csb;
     uint8_t pin_cmd;
     uint8_t pin_fault;
-    
-    uint8_t count;
 
     inline void beginTransaction();
     inline void endTransaction();
 
     inline void sendCmd(uint8_t);
 
+    inline uint8_t transfer8(uint8_t addr, uint8_t data);
+    inline uint32_t transfer32(uint8_t addr, uint32_t data);
+
 public:
 
     MAX22200(uint8_t en, uint8_t csb, uint8_t cmd);
-    MAX22200(uint8_t en, uint8_t csb, uint8_t cmd, uint8_t count);
 
     inline void enable();
     inline void disable();
     inline void setEnable(bool);
 
-    inline void read(uint8_t addr, uint8_t* data);
-    inline void read(uint8_t addr, uint32_t* data);
+    inline uint8_t  read8(uint8_t addr);
+    inline uint32_t read32(uint8_t addr);
 
-    inline void write(uint8_t addr, const uint8_t* data);
-    inline void write(uint8_t addr, const uint32_t* data);
+    inline void write8(uint8_t addr, uint8_t data);
+    inline void write32(uint8_t addr, uint32_t data);
 
 };
 
